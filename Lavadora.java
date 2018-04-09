fdeac31a985f92e7f4159fa75274390e6f499468
isabelRG <35698348+isabelRG@users.noreply.github.com>
2018-04-09 08:28:22 -0500

    test.java

---
+++ Lavadora.java
@@ -0,0 +1,27 @@
+public class Lavadora extends Electrodomestico{
+
+  private int carga;
+
+  private final static int CARGA_DEF=5;
+
+  public Lavadora(){
+       this(PRECIO_BASE_DEF, PESO_DEF, CONSUMO_ENERGETICO_DEF, COLOR_DEF, CARGA_DEF);
+     }
+  public Lavadora(double precioBase, double peso){
+          this(precioBase, peso, CONSUMO_ENERGETICO_DEF, COLOR_DEF, CARGA_DEF);
+      }
+  public Lavadora(double precioBase, double peso, char consumoEnergetico, String color, int carga){
+           super(precioBase, peso, consumoEnergetico, color);
+           this.carga=carga;
+       }
+  public int getCarga() {
+             return carga;
+           }
+  public double precioFinal(){
+                  double plus=super.precioFinal();
+                  if (carga>30){
+                      plus+=50;
+                    }
+                  return plus;    
+              }
+            }
---
+++ Television.java
@@ -0,0 +1,27 @@
+public class Television extends Electrodomestico{
+    private int resolucion;
+    private boolean sintonizadorTDT;
+    private final static int RESOLUCION_DEF=20;
+
+    public Television(){
+       this(PRECIO_BASE_DEF, PESO_DEF, CONSUMO_ENERGETICO_DEF, COLOR_DEF, RESOLUCION_DEF, false);
+   }
+   public Television(double precioBase, double peso){
+        this(precioBase, peso, CONSUMO_ENERGETICO_DEF, COLOR_DEF, RESOLUCION_DEF, false);
+    }
+    public Television(double precioBase, double peso, char consumoEnergetico, String color, int resolucion, boolean sintonizadorTDT){
+       super(precioBase, peso, consumoEnergetico, color);
+       this.resolucion=resolucion;
+       this.sintonizadorTDT=sintonizadorTDT;
+   }
+   public double precioFinal(){
+       double plus=super.precioFinal();
+       if (resolucion>40){
+           plus+=precioBase*0.3;
+       }
+       if (sintonizadorTDT){
+           plus+=50;
+       }
+       return plus;
+   }
+}
